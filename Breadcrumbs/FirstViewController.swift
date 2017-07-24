a9c8663d29485a724c5863bf9a72262be0aa2f09
iD Student <idstudent@iDs-iMac.local>
2017-07-24 16:43:51 -0700

    Initial Commit

---
+++ Breadcrumbs/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Breadcrumbs
+//
+//  Created by iD Student on 7/24/17.
+//  Copyright © 2017 iD Tech. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
